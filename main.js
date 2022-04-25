398867828af9bd16e97767442110f4c6a58d1a09
Howard Kirtley <91100329+kirtleyHD@users.noreply.github.com>
2022-04-25 18:11:44 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+// declaring variable myAge and assigning it as 34
+var myAge = 34;
+
+// changable variable 'earlyYears' with a value of 2
+var earlyYears = 2;
+
+earlyYears *= 10.5;
+
+// setting 'laterYears' to myAge minus 2;
+var laterYears = myAge - 2;
+
+// multiplying laterYears by 4 and reassigning it
+laterYears *= 4;
+
+// Add earlyYears and laterYears together, and store that in a variable named myAgeInDogYears.
+var myAgeInDogYears = earlyYears + laterYears;
+
+// 'myName' as a string and converted to lowercase with the .toLowerCase method
+var myName = 'Danny';
+myName.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} in human years which is ${myAgeInDogYears} years old in dog years.`)
+// console logging the given statement using string iterpolation (aka object literals) to display given values
